f7b4ec8432246a193b96a81e3b12b1125d383bfb
pedro roca de togores <pedrobgrdt@gmail.com>
2024-01-18 23:54:49 +0100

    Lab finito con dos fallos negociables...

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,15 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    }
+    if (num1 < num2) {
+        return num2
+    }
+    else {
+        return num1 || num2
+    }
+}
 
 
 
@@ -7,15 +17,48 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arrayofWords) {
 
+    let longestWordInArray = ""
+
+    if (arrayofWords.length === 0) {
+        return 0
+    }
+
+    for (let i = 0; i < arrayofWords.length; i++) {
+        if (arrayofWords[i].length > longestWordInArray.length) {
+            longestWordInArray = arrayofWords[i]
+        }
+    }
+    return longestWordInArray
+
+}
+console.log(findLongestWord(words))
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arrayOfNumbers) {
+
+    let totalSum = 0
+
+    if (arrayOfNumbers.length === 0) {
+        return 0
+    }
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        totalSum += arrayOfNumbers[i]
+
+    }
+
+    return totalSum
+}
+
+
+
+
 
 
 
@@ -23,7 +66,27 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(averageNumbers) {
+
+    if (averageNumbers.length === 0) {
+        return 0
+    }
+    let arraySum = 0
+
+    for (let i = 0; i < averageNumbers.length; i++) {
+        arraySum += averageNumbers[i]
+        return arraySum
+    }
+
+
+
+    const result = arraySum / averageNumbers.length
+
+    return result
+    ///no entiendoo como no sale
+
+}
+
 
 
 
@@ -31,4 +94,23 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(list) {
+    if (list.length === 0) {
+        return null
+    }
+
+    for (let i = 0; i < list.length; i++) {
+        if (list.length[i] !== list) {
+            return false
+        }
+        else if (list.length[i] === list) {
+            return true
+        } // como es que no sale, si es lo mismo que el return false...
+
+    }
+
+
+
+}
+
+
