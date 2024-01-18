01078322b240abc22d84fc0bc0dc96661b20f640
Pedro Cardoso <pedrocrds110@gmail.com>
2024-01-18 14:49:47 +0000

    functions and arrays completed

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,49 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(number1, number2) {
+    if (number1 > number2){
+        return number1
+    }
+    else if (number1 < number2){
+        return number2
+    }
+    else{
+        return number1
+    }
+}
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
+function findLongestWord(words) {
+    let longestWord =""
+    if (words.length === 0){
+        return 0
+    }
+    if (words.length === 1){
+        return words[0];
+    }
+    words.forEach((word)=>{
+        if(word.length > longestWord.length){
+            longestWord = word;
+        }
+    })
+    return longestWord
+}
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(nums) {
+    let sum = 0
+    for (let i = 0; i<nums.length; i++){
+        sum+= nums[i]
+    }
+
+    return sum
+}
 
 
 
@@ -23,12 +51,26 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
+function averageNumbers(nums) {
+    if (nums.length === 0){
+        return 0
+    }
+    return sumNumbers(nums)/nums.length;
+}
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords, wordToSearch) {
+    if (arrayOfWords.length === 0 || wordToSearch.length === 0){
+        return null;
+    }
+    for (let i = 0; i < arrayOfWords.length; i++) {
+        if (arrayOfWords[i] === wordToSearch) {
+            return true;
+        }
+    }
+
+    return false;
+}
