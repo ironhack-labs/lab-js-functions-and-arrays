45a16431cb867ab469d3f5b9f31d54684c1953b2
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-01-18 15:19:14 +0100

    done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,39 @@
-// Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
 
+
+
+// Iteration 1 | Find the Maximum
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } 
+    else if (num1 < num2) {
+        return num2;
+    }
+    else {
+        return num1, num2
+    }    
+}
 
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+
+    let count = 0;
+    let longestWord = 0;
+
+    for (let i = 0; i < words.length; i++) {
+
+        if(words[i].length > count) {
+            count = words[i].length;
+            longestWord = words[i]
+        }
+    }
+
+    return longestWord
+}
 
 
 
@@ -15,15 +41,33 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
 
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+
+    return sum;
+}
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    let sum = sumNumbers(numbers2);
+    let size = numbers2.length
+    let average = sum / size;
+
+    if (size === 0) {
+        return 0;
+    }
+    
+    return average;
+}
 
 
 
@@ -31,4 +75,17 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, word) {
+
+    if (words2.length === 0) {
+        return null
+    }
+    
+    else if (words2.includes(word)) {
+        return true
+    }
+
+    else if (!words2.includes(word) ) {
+        return false
+    }
+}
