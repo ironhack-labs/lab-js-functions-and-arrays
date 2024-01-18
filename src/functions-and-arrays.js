2d191fadba670c315a295fa8407547a4e98c2b5c
Rui Marques <ruimarques0826@gmail.com>
2024-01-18 20:21:27 +0000

    lab solved

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,17 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(value1, value2) {
+    if (value1 > value2) {
+        return value1;
+    }
+    else if (value1 < value2) {
+        return value2;
+    }
+    else {
+        return value1;
+    }
+}
+
+maxOfTwoNumbers(5, 3);
 
 
 
@@ -7,7 +19,20 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(longest) {
+    let longestWord = "";
+    longest.forEach((name) => { 
+        if (name.length > longestWord.length) {
+        longestWord = name;
+        }
+    });
+    if (longestWord === "") {
+        return 0;
+    }
+    return longestWord;
+}
+
+findLongestWord(words);
 
 
 
@@ -15,7 +40,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(sum) {
+    let sumAll = 0;
+    for (let i = 0; i < sum.length; i++) {
+        sumAll += sum[i];
+    }
+    return sumAll;
+}
+
 
 
 
@@ -23,7 +55,10 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arrNum) {
+    if(arrNum.length === 0) return 0
+        return sumNumbers(arrNum) / arrNum.length;
+}
 
 
 
@@ -31,4 +66,15 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordArr, word) {
+    if (wordArr.length === 0) {
+        return null;
+    }
+    for (let i = 0; i < wordArr.length; i++) {
+        if (wordArr[i] === word) {
+            return true;
+        }
+ }
+    return false;
+}
+doesWordExist(words2, "trouble");
