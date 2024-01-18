504c283273054ca6a04a75a2654c2e79a6615e97
Vituin <124085647+Vituin@users.noreply.github.com>
2024-01-18 21:54:18 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,31 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(Value1, Value2) {
+    if (Value1 > Value2)
+        return Value1
 
+    if (Value2 > Value1)
+        return Value2
 
+    if (Value1 === Value2)
+        return Value1
+}
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    let longword = ""
+    if (!words.length) return 0
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longword.length) {
+            longword = words[i]
+
+        }
+    }
+    return longword
+
+}
 
 
 
@@ -15,7 +33,15 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+    }
+    return sum
+
+}
+
 
 
 
@@ -23,7 +49,17 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    let sum = 0
+    if (!numbers2.length) return 0
+    for (let i = 0; i < numbers2.length; i++) {
+        sum += numbers2[i]
+    }
+    return sum / numbers2.length
+
+
+}
+
 
 
 
@@ -31,4 +67,4 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist() { }
