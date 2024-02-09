4fa4d19b336256b4c160d9c5d275773c38ac5f26
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-02-09 10:45:32 -0500

    lab done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,29 +1,56 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1
+    } else if (num2 > num1) {
+        return num2
+    } else {
+        return num1
+    }
+}
 
 
 
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+// const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+
+    let longestWord = ''
+    words.forEach((word) => {
+        if (word.length > longestWord.length) {
+            longestWord = word
+        }
+    })
+
+    return longestWord ? longestWord : 0
+
+}
 
 
 
 
 // Iteration 3 | Sum Numbers
-const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+// const theseNnumbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let counter = 0
+    numbers.forEach((number) => {
+        counter += number
+    })
+    return counter
+}
 
 
 
 
 // Iteration 4 | Numbers Average
-const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+// const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+    return numbers.length ? sumNumbers(numbers)/numbers.length : 0
+}
 
 
 
@@ -31,4 +58,18 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+    if (arr.length < 1) {
+        return null
+    }
+
+    let exists = false
+
+    arr.forEach((element) => {
+        if (element === word) {
+            exists = true
+        }
+    })
+
+    return exists
+}
