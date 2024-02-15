a1cee2abc509fc4bb23c3d44dce11f1823737034
mohamed1410 <m.hossam3330@yahoo.com>
2024-02-15 15:47:34 +0100

    lab is done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,12 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1 ,num2) {
+   if (num1> num2){
+    return num1 ;
+   } else if (num2> num1) {
+    return num2;
+   } else{ return num1 }
+}
+
 
 
 
@@ -7,7 +14,18 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length ===0){
+        return 0 } else if (words.length===1){
+            return words[0]}else{
+                let longestWord = words[0]
+                for (let i=1; i < words.length; i++){
+                    if (words[i].length> longestWord.length){
+                        longestWord = words[i]
+                    }
+                }
+                return longestWord
+            }   }
 
 
 
@@ -15,7 +33,12 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0
+    for ( let i = 0 ; i < numbers.length; i++){
+        sum += numbers[i]}
+        return sum
+}
 
 
 
@@ -23,7 +46,13 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+    if (numbers.length===0){
+        return 0} else{
+            const sum = sumNumbers(numbers)
+            return sum / numbers.length
+        }
+}
 
 
 
@@ -31,4 +60,9 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words, trget) {
+    if (words.length===0){
+        return null}else{
+           return words.includes(trget)
+        }
+}
