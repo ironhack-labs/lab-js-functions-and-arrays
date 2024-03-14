f7116497da835e82791b4aa25bc8a356bddbb63e
KemalGokten <ukemalgokten@gmail.com>
2024-03-14 12:11:37 +0100

    Solve the questions

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,7 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+    return number1 >= number2 ? number1 : number2;  
+}
 
 
 
@@ -7,7 +9,20 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+   let longestWord = '';
+
+    words.forEach(word => {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    });
+
+    return longestWord ? longestWord : 0;
+
+    // One way to do it in one line.
+    //return words.sort((a, b) => b.length - a.length)[0] ? words[0] : 0;
+}
 
 
 
@@ -15,7 +30,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let count = 0;
+    numbers.forEach(number => count+=number);
+    return count;
+
+    // one way to do it in one line.
+    //return numbers.reduce((a, b) => a + b, 0);
+}
 
 
 
@@ -23,7 +45,9 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    return numbers2.length ? numbers2.reduce((a, b) => a + b) / numbers2.length : 0;
+}
 
 
 
@@ -31,4 +55,8 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, word) {
+    return words2.length ? words2.includes(word) : null;
+}
+
+
