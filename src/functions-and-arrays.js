42a84f6b31f9baf93e9e24695cc8caae300661e8
Celso Gando <Celsogando7@gmail.com>
2024-03-17 23:09:55 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,16 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1 , num2) {
+    if(num1 > num2){
+
+        return num1;
+    }
+    if(num2 > num1){
+        return num2;
+    }
+    return num1;
+
+   // return num1>num2  ? num1: num2
+}
 
 
 
@@ -7,7 +18,20 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    let max = "";
+
+    if(words.length === 0){
+        return 0;
+    }
+
+    for(const element of words){
+        if(element.length > max.length){
+            max = element;
+        }
+    }
+    return max;
+}
 
 
 
@@ -15,7 +39,13 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+    for(const element of numbers){
+        sum += element;
+    }
+    return sum;
+}
 
 
 
@@ -23,7 +53,12 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+    if(numbers.length === 0){
+        return 0;
+    }
+    return sumNumbers(numbers)/ numbers.length;
+}
 
 
 
@@ -31,4 +66,14 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(searchArray, searchWord) {
+    if(searchArray.length !== 0){
+        for(const element of searchArray){
+            if(element === searchWord){
+                return true; 
+            }
+        }
+        return false;
+    }
+    return null;
+}
