b33ba7388e03cb7f0db001e14235da7d1a7bb826
Laura Sinclair <l.sinclair@tuta.io>
2024-03-28 14:45:28 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,11 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(number1, number2) {
+    if (number1 > number2) {
+        return number1
+    } else {
+        return number2
+    }
+}
 
 
 
@@ -7,23 +13,49 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    var lgth = 0;
+    var longest;
 
+    if (arr.length === 0) {
+        return null
+    } else if (arr.length > 0) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].length > lgth) {
+                var lgth = arr[i].length;
+                longest = arr[i];
+            }
+        }
+        return longest
+    }
+}
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
+function sumNumbers(arr) {
+    let sum = 0;
+    for (value of arr) {
+        sum += value
+    }
+    return sum
+}
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+    if (arr.length == 0) {
+        return 0
+    } else {
+        let average = sumNumbers(arr) / arr.length;
+        return average;
+    }
+}
 
 
 
@@ -31,4 +63,15 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr, wordToSearch) {
+    if (arr.length == 0) {
+        return null
+    } else {
+        for (value of arr) {
+            if (value === wordToSearch) {
+                return true
+            }
+        }
+        return false
+    }
+}
