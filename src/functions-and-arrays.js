8266a7b54a3d3366c802168083c8757ca43109ac
ecemonkol <ecemonkol@gmail.com>
2024-03-28 15:51:13 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,94 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else if (num2 === num1) {
+    return num1;
+  }
+}
 
-
-
+maxOfTwoNumbers(9, 21);
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+  if (arr.length === 0) {
+    return null;
+  } else if (arr.length === 1) {
+    return arr[0];
+  }
 
+  let lengthArray = arr.map((item) => item.length);
+  const longestWordIndex = lengthArray.indexOf(Math.max(...lengthArray));
+  return arr[longestWordIndex];
+}
 
-
+findLongestWord(words);
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+  let sum = 0;
+  if (arr.length === 0) {
+    return 0;
+  } else if (arr.length === 1) {
+    return arr[0];
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+  }
+  return sum / arr.length;
+}
 
-
-
+averageNumbers(numbers2);
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+console.log(doesWordExist(words2, "machine"));
+
+function doesWordExist(arr, str) {
+  if (!arr.length) return null;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.includes(str)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+doesWordExist(words2, str);
