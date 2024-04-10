db2defa85b80591c346a594e82ab6063016b7677
Asier MD <asier.md09@gmail.com>
2024-04-10 15:04:00 +0200

    Solved Lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,48 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(par1, par2) {
+    return Math.max(par1, par2)
+}
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    let largoInicial = 0;
+    let maxLongitud;
+    if(arr.length==0){
+        maxLongitud=null
+    }
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].length > largoInicial) {
+        largoInicial = arr[i].length;
+        maxLongitud = arr[i];
+      }
+    }
+    
+    return maxLongitud
+}
 
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+let respuesta=0;
+function sumNumbers(numeros) {
 
-function sumNumbers() {}
+    if(numeros.length>1){
+        for(let i=0;i<numeros.length;i++){
+            respuesta+=numeros[i]
+        }
+    }else if(numeros.length==1){
+        respuesta=numeros[0]
+    }else if(numeros.length==0){
+        respuesta=0
+    }
+
+    return respuesta
+}
 
 
 
@@ -23,12 +50,34 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numeros2) {
+    let respuesta2
+    let suma=sumNumbers(numeros2)//suma es el resultado de la funcion sumNumbers//redondear
+
+    if(numeros2.length==0){
+        respuesta2=0
+    }else if(numeros2.length==1){
+        respuesta2 = suma / numeros2.length //media
+    }else if(numeros2.length>1){
+        respuesta2 = suma / numeros2.length //media
+    }
+ 
+    return Math.floor(respuesta2)
+}
 
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
-
-function doesWordExist() {}
+let respuesta3
+function doesWordExist(arr, word) {
+    if(arr.length==0){
+        respuesta3 = null
+    }else if(arr.indexOf(word)!==-1){
+        respuesta3=true
+    }else{
+        respuesta3=false
+    }
+    return respuesta3
+}
