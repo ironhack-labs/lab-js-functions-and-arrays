336114c0c9756edd73ca9dd365df1bb1ca5fa5f2
Francisco Soares <franciscosoares13@hotmail.com>
2024-04-11 20:45:35 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,21 +1,58 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1,num2) {
+    if(num1<num2) {
+    return num2;
+}
+else if (num1>num2) {
+    return num1;
+}
+else {
+    return num1 || num2;
+}
+}
+console.log(maxOfTwoNumbers(1,2));
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
-
-
+function findLongestWord(words) {
+    
+    if (words.length === 0) {
+        return null;
+    }
+    let bigger = " ";
+    for (let i =0; i < words.length; i++) {
+        
+        if (words[i].length > bigger.length) {
+            bigger= words[i];
+    }
+    console.log(bigger);
+    
+    }
+    return bigger;
+}
+findLongestWord(words);
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(array) {
+    let sum = 0;
+    if(array.length === 0){
+        return 0;
+    }
+    else if (array.length === 1) {
+        return array[0];
+    }
+    
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum; 
+
+}
+console.log(sumNumbers(numbers));
 
 
 
@@ -23,7 +60,21 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(array) {
+    let sum = 0;
+
+    if(array.length === 0){
+        return 0;
+    }
+    else if (array.length === 1) {
+        return array[0];
+    }
+    
+    for(let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum/array.length; 
+}
 
 
 
@@ -31,4 +82,16 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr,word) {
+    if (arr.length === 0) {
+        return null;
+    }
+
+    if(arr.indexOf(word) === -1) {
+        return false
+    }
+    else {
+        return true;
+    }
+
+}
