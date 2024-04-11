de3ec51009ad92820c97cd3ea136e95e6405bbd3
Lafaiete Santos <lafaiete21@icloud.com>
2024-04-11 16:13:19 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,29 +1,73 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(arg1, arg2) {
 
-
+    if (arg1 > arg2) {
+        return arg1;
+    } else if (arg2 > arg1) {
+        return arg2;
+    } else {
+        return arg1;
+    }
+}
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(wordArray) {
 
+    if (wordArray.length === 0) {
+        return null;
+    }
 
-
+    let longestWord = '';
+    for (let word of wordArray) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbersArray) {
+    if (numbersArray.length === 0) {
+        return 0;
+    }
 
+    if (numbersArray.length === 1) {
+        return numbersArray[0];
+    }
+
+    const sum = numbersArray.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0);
+
+    return sum;
+}
 
 
 
 // Iteration 4 | Numbers Average
-const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
+function averageNumbers(numbersArray) {
+    if (numbersArray.length === 0) {
+        return 0;
+    }
 
-function averageNumbers() {}
+    if (numbersArray.length === 1) {
+        return numbersArray[0];
+    }
+
+    const sum = numbersArray.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0);
+
+    const average = sum / numbersArray.length;
+
+    return average;
+}
 
 
 
@@ -31,4 +75,11 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordArray, word) {
+ 
+    if (wordArray.length === 0) {
+        return null;
+    }
+
+    return wordArray.includes(word);
+}
