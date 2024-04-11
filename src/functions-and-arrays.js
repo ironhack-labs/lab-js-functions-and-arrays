f8c95dfc3f9d88c3f68ed3c9788808e61560e919
Edir Sequeira <edirjcs@gmail.com>
2024-04-11 23:57:21 +0100

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,120 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else if (num2 > num1) {
+    return num2;
+  } else {
+    return num1 || num2;
+  }
+}
+console.log(maxOfTwoNumbers(5, 10));
+console.log(maxOfTwoNumbers(20, 5));
+console.log(maxOfTwoNumbers(10, 10));
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null;
+  }
 
+  if (words.length === 1) {
+    return words[0];
+  }
+
+  let longestWord = words[0];
+
+  for (let i = 1; i < words.length; i++) {
+    if (words[i].length > longestWord.length) {
+      longestWord = words[i];
+    }
+  }
+
+  return longestWord;
+}
+console.log(findLongestWord(words));
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
 
+  if (numbers.length === 1) {
+    return numbers[0];
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+console.log(sumNumbers(numbers));
 
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+  if (numbers2.length === 0) {
+    return 0;
+  }
 
+  if (numbers2.length === 1) {
+    return numbers2[0];
+  }
 
+  let sum = 0;
+  for (let i = 0; i < numbers2.length; i++) {
+    sum += numbers2[i];
+  }
 
+  const average = sum / numbers2.length;
+    return average;
+}
+console.log(averageNumbers(numbers2));
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(arr, word) {
+    if (arr.length === 0) {
+        return null;
+    }
+
+    let wordExist = false;
+
+    arr.forEach((element) => {
+        if (element === word) {
+            wordExist = true;
+        }
+    });
+    return wordExist;
+    
+}
