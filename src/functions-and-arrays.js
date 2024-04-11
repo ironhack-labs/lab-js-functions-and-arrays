30665aa2c12a269023f2faa460c97e79d196ed73
Marie Rabel <marie.rabel0697@gmail.com>
2024-04-11 16:10:31 +0200

    lab fini

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,12 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(nbr1, nbr2) {
+    if (nbr1>nbr2) {
+        return nbr1;
+    }
+    else { 
+        return nbr2;
+    }
+}
 
 
 
@@ -7,7 +14,22 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    let result = "";
+
+if (arr.length === 0) {
+ return null;
+}
+
+    for (const mot of arr) {
+        console.log(mot);
+        if (mot.length > result.length) {
+            result = mot;
+        }
+    }
+
+    return result;
+}
 
 
 
@@ -15,7 +37,13 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr) {
+    ajout = 0
+    for (const num of arr) {
+        ajout += num;
+    }
+    return ajout;
+}
 
 
 
@@ -23,7 +51,14 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(arr) {
+    if (arr.length === 0) {
+        return 0;
+       }
+    let total = sumNumbers(arr);
+    aver = total / arr.length;
+    return aver;
+}
 
 
 
@@ -31,4 +66,16 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr, toFind) {
+
+    if (arr.length === 0) {
+        return null;
+       }
+
+   for (const word of arr) {
+    if (word === toFind) {
+        return true;
+    }
+   }
+  return false;
+}
