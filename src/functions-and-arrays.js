0083482873dd01222f6101e703e02dae780bfaf9
Farnoush Daliran <farnoushfdf@gmail.com>
2024-05-09 14:35:35 +0200

    commit Function and Array lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,11 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if(num1 > num2){
+        return num1;
+    } else {
+        return num2;
+    }
+}
 
 
 
@@ -7,7 +13,19 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    let longestWord = words[0];
+    if (words.length === 0){
+        return null;
+    } else {
+        for(let i = 0; i < words.length; i++) {
+            if (words[i].length > longestWord.length) {
+                longestWord = words[i];
+            }
+        }
+        return longestWord;
+    }
+}
 
 
 
@@ -15,7 +33,13 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
 
 
 
@@ -23,12 +47,35 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(numbers) {
+    let sum = 0;
+    if (numbers.length === 0) {
+        return 0;
+    } else {
+        for (let i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        let average = sum / numbers.length;
+        return average;
+    }
+    
+}
+// console.log(averageNumbers(numbers2))
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words, wordForSearching) {
+    if (words.length === 0) {
+        return null;
+    } else {
+        for (let i = 0; i < words.length; i++) {
+            if (words[i] === wordForSearching) {
+                return true;
+            }
+        }
+         return false;
+    }
+}
