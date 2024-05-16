04bf2eb1a204b6b47f09969f0593619e06d850e2
Sieur Loic <loic.tramis@gmail.com>
2024-05-16 11:53:40 +0200

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,57 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(a, b) {
+    return a < b ? b : a;
+}
+console.log(maxOfTwoNumbers(50, 10));
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
-
-
+function findLongestWord(words) {
+    let longestWord = "";
+    if (!words) {
+        return null;
+    }
+    for (let index = 0; index < words.length; index++) {
+        longestWord = longestWord.length < words[index].length ? words[index] : longestWord;
+    }
+    return longestWord;
+}
+console.log(findLongestWord(words));
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let index = 0; index < numbers.length; index++) {
+        sum += numbers[index];
+    }
+    return sum;
+}
+console.log(sumNumbers(numbers));
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(numbers2) {
+    const sum = sumNumbers(numbers2);
+    return sum / numbers2.length;
+}
+console.log(averageNumbers(numbers2));
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words, word) {
+    if (!words) {
+        return null;
+    }
+    for (let index = 0; index < words.length; index++) {
+        if (word === words[index]) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(doesWordExist(words2, "subsets"));
