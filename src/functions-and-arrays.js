27adc62490149823171caa214381e8f8ad3b4613
maria magdalena <neumariamagdalena@gmail.com>
2024-06-06 11:17:05 +0200

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,18 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    let returnvalue 
+    if (num1 > num2) {
+        returnvalue = num1
+        } else if (num2 > num1) {
+            returnvalue =num2 
+        } else {
+            returnvalue = num1; // or num2, since they are equal
+        }
+        return returnvalue;
+    }
+    maxOfTwoNumbers(2,12);
+    console.log(maxOfTwoNumbers(2,12))
+
 
 
 
@@ -7,7 +20,16 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0) return null;
+    let longestWord = words[0];
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i]
+        }
+    }
+    return longestWord
+}
 
 
 
@@ -15,7 +37,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    if (numbers.length === 0) return 0;
+    let sum = 0;
+    for (i = 0; i < numbers.length; i++) {
+        sum = sum + numbers[i];
+    }
+    return sum;
+}
 
 
 
@@ -23,7 +52,16 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) return 0;
+    let sum2 = 0
+    let average = 0;
+    for (i=0; i<numbers2.length; i++) {
+        sum2 = sum2 + numbers2[i];
+    }
+    average = sum2 / numbers2.length;
+    return average;
+}
 
 
 
@@ -31,4 +69,12 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, currentWord) {
+    if (words2.length === 0) return null
+    for (let i = 0; i < words2.length; i++ ) {
+        if (currentWord === words2[i]) {
+            return true; 
+        } 
+    }
+    return false;
+}
