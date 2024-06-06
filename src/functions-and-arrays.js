b0cacf22caa9cecba0b2f1efd92a49643aeec3e0
Kubilay ADA <kubilayada.26@gmail.com>
2024-06-06 15:58:32 +0200

    Solved Lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,13 +1,32 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
+function maxOfTwoNumbers(num1,  num2) {
+if (num1 > num2){
+    return(num1);
+}
+if (num2 > num1){
+    return(num2);
+}
+else {
+    return(num1, num2);
+}
+}
 
 
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words){
+    if (words.length === 0){
+        return null;
+    }
+    let longestWord = "";
+    for (let i = 0; i < words.length; i++){
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
+        }
+    }
+    return (longestWord);
+}
 
 
 
@@ -15,20 +34,39 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
+function sumNumbers(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+    return(sum);
+ }
 
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers) {
+    if (numbers.length === 0){
+        return 0;
+    }
+    let sum = sumNumbers(numbers);
+    let average = sum / numbers.length;
+    return(average);
 
-
-
-
+}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words, searchWord) {
+    if(words.length === 0){
+        return null;
+    }
+    for (i = 0; i < words.length; i++){
+        if (words[i] === searchWord){
+           return true;
+        }
+    }
+return false;
+}
+
